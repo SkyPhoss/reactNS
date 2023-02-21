02cb29b08db4b0755bb9415a1b88c72e43b79f9c
ryanfrynelson13 <ryanfrynelson@hotmail.fr>
2023-02-21 11:09:25 +0100

    login

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
+import { useCallback, useState } from 'react'
+import Login from './components/login/login'
 import './App.css'
 
 function App() {
+  const [display, setDisplay] = useState('login')
+  const [login, setLogin] = useState('')
 
-  return (
+  const getLogin = useCallback((login) => {
+    setLogin(login)
+    setDisplay('category')
+  })
+
+  return display === 'login'? (
     <>
-    
+      <Login onLogin={getLogin}/>
+    </>
+  ) : display === 'category'? (
+    <>
+      <p></p>
+    </>
+  ) : (
+    <>
     </>
   )
+
+ 
 }
 
 export default App
